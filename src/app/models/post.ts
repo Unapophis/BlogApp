8368298e34073781367f530a27c4f629beb3de8a
Julien Le Beau <azedpoij@gmail.com>
2018-08-15 00:14:54 +0200

    Modifications pour exercice openclassroom

---
+++ src/app/models/post.ts
@@ -0,0 +1,10 @@
+export class Post {
+    created_at: Date;
+
+    constructor(
+        public title: string,
+        public content: string,
+        public loveIts: number) {
+            this.created_at = new Date();
+         }
+  }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Post } from '../models/post';
+import * as firebase from 'firebase';
+
+@Injectable()
+export class PostsService {
+
+    posts: Post[] = [];
+    postsSubject = new Subject<Post[]>();
+
+    constructor() { }
+
+    emitPosts() {
+        this.postsSubject.next(this.posts);
+    }
+
+    savePosts() {
+        firebase.database().ref('/posts').set(this.posts);
+    }
+
+    getPosts() {
+        firebase.database().ref('/posts')
+        .on('value', (data) => {
+        this.posts = data.val() ? data.val() : [];
+        this.emitPosts();
+        })
+    }
+
+    getSinglePosts(id: number) {
+        return new Promise(
+        (resolve, reject) => {
+            firebase.database().ref('/posts/' + id).once('value').then(
+            (data) => {
+                resolve(data.val());
+            }, (error) => {
+                reject(error);
+            }
+            );
+        }
+        );
+    }
+
+    createNewPost(newPost: Post) {
+        this.posts.push(newPost);
+        this.savePosts();
+        this.emitPosts();
+    }
+
+    removePost(post: Post) {
+        const postIndexToRemove = this.posts.findIndex(
+        (postEl) => {
+            if (postEl === post) {
+            return true;
+            }
+        }
+        );
+        this.posts.splice(postIndexToRemove, 1);
+        this.savePosts();
+        this.emitPosts();
+    }
+
+    loveIt(id: number) {
+        this.getSinglePosts(+id).then(
+            (post: Post) => {
+              this.editPost(post, id, true, false);              
+            }
+        )
+    }
+
+    dontLoveIt(id: number) {
+        this.getSinglePosts(+id).then(
+            (post: Post) => {
+              this.editPost(post, id, false, true);              
+            }
+        )
+    }
+
+    editPost(post: Post, id: number, love: boolean, dontLove: boolean) {
+        if (love) {
+            this.posts[id].loveIts ++;
+        }
+
+        if (dontLove) {
+            this.posts[id].loveIts --;
+        }
+
+        this.savePosts();
+        this.emitPosts();
+    }
+}
